3dba8d10e62a9f11c81f85c35bc467d491381c9a
Michal Siminski <michal.siminski3@gmail.com>
2021-02-03 09:19:53 +0100

    add animation to menu button

---
+++ src/js/menuButton.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.menu-button');
+const navigation = document.querySelector('.navigation');
+let flag = true;
+
+const showAndCloseMenu = () => {
+    if(flag){
+    TweenMax.to('.navigation',{duration: 2, y: 0, ease: "bounce.out"});
+    flag = !flag;
+}else{
+    TweenMax.to('.navigation',{duration: 1, y: -window.innerHeight});
+    flag = !flag; 
+}}
+
+button.addEventListener('click', showAndCloseMenu);
